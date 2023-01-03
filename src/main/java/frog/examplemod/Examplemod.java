49db57f697f28c4be8e19c4d13dbdf8bf11630f5
1202-Ledesma-Jordan <jledesma@nevada.unr.edu>
2023-01-02 16:45:52 -0800

    Set up example mod

---
+++ src/main/java/frog/examplemod/Examplemod.java
@@ -0,0 +1,10 @@
+package frog.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Examplemod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/frog/examplemod/client/ExamplemodClient.java
@@ -0,0 +1,14 @@
+package frog.examplemod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ExamplemodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
